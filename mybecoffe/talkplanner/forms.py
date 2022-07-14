2af9a44682198e67d811381dbacdf34c8a5f521d
Eric Chagrikian <echagrikian@hotmail.com>
2022-07-14 15:52:53 +0200

    test heroku migrate

--- mybecoffe/talkplanner/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Talks
-
-class TalkForm(forms.ModelForm):
-	class Meta:
-		model = Talks
-		fields="__all__"
--- mybecoffe/talkplanner/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index,name='talkplanner'),
-    path('del/<str:item_id>',views.remove, name="del"),
-]
